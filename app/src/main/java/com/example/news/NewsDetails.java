e93e2cbe76831f2f5bf2a197bdfc17ab7a798372
Marianne <marianne_zayed@hotmail.com>
2016-11-26 18:03:48 +0200

    finish details fragament UI and support up button

--- app/src/main/java/com/example/news/NewsDetails.java
+++ app/src/main/java/com/example/news/NewsDetails.java
@@ -3,8 +3,10 @@ package com.example.news;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class NewsDetails extends AppCompatActivity {
@@ -15,8 +17,18 @@ public class NewsDetails extends AppCompatActivity {
         setContentView(R.layout.activity_news_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
